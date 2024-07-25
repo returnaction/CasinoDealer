a3e0a8a17da3283b29550ad85d1a4eacad50d0a8
returnaction <obergannv@gmail.com>
2024-07-25 16:27:33 -0400

    Add UserAnswer model

---
+++ CasinoDealer.Models/Models/QuestionModels/UserAnswer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace CasinoDealer.Models.Models.QuestionModels
+{
+    public class UserAnswer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string  UserId { get; set; } = null!;
+
+        public IdentityUser User { get; set; } = null!;
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; } = null!;
+
+        [Required]
+        public string Answer { get; set; } = null!;
+
+        [Required]
+        public bool IsCorrect { get; set; } 
+    }
+}
